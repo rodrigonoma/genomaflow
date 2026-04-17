e6bfd0d7d62e815024c92141b09ab3f2dd774b80
GenomaFlow Dev <dev@genomaflow.com>
2026-04-17 03:36:41 -0300

    fix: rename FK constraint exams_patient_id_fkey to exams_subject_id_fkey

--- apps/api/src/db/migrations/012_patients_to_subjects.sql
+++ apps/api/src/db/migrations/012_patients_to_subjects.sql
@@ -7,3 +7,5 @@ ALTER TABLE subjects
   ADD COLUMN owner_cpf_hash TEXT;
 
 ALTER TABLE exams RENAME COLUMN patient_id TO subject_id;
+
+ALTER TABLE exams RENAME CONSTRAINT exams_patient_id_fkey TO exams_subject_id_fkey;
